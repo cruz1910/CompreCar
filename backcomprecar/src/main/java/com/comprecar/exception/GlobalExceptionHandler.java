2e602be6b273b3e120ed05e35116743e4df9e378
Lucas Cruz <cruzzzlucas1910@gmail.com>
2025-06-27 01:03:19 -0300

    crição e ajuste de codigos

---
+++ backcomprecar/src/main/java/com/comprecar/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.comprecar.exception;
+
+
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsuarioException.class)
+    public ResponseEntity<Map<String, String>> handleUsuarioException(UsuarioException ex) {
+        // Retorna JSON com a chave "message" e o texto da mensagem do erro
+        return new ResponseEntity<>(
+            Map.of("message", ex.getMessage()),
+            HttpStatus.BAD_REQUEST
+        );
+    }
+   
+
+}
