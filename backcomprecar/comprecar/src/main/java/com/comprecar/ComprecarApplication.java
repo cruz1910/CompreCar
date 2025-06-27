955c2138a0ed1d62e0b4888117ddb8acc7af78b6
Lucas Cruz <cruzzzlucas1910@gmail.com>
2025-06-26 22:05:02 -0300

    criação CompreCar

---
+++ backcomprecar/comprecar/src/main/java/com/comprecar/ComprecarApplication.java
@@ -0,0 +1,13 @@
+package com.comprecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprecarApplication.class, args);
+	}
+
+}
---
+++ backcomprecar/comprecar/src/test/java/com/comprecar/ComprecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comprecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
