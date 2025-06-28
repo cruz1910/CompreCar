8e74aaeaeb3ad9ba76aee78f5f89d074724263ba
Milton Júnior <jrsinhorini@gmail.com>
2025-06-28 19:20:19 -0300

    implementação do veiculoDTO

--- backcomprecar/src/main/java/com/comprecar/dto/VeiculoDTO.java
+++ backcomprecar/src/main/java/com/comprecar/dto/VeiculoDTO.java
@@ -1,5 +1,96 @@
 package com.comprecar.dto;
 
+import java.math.BigDecimal;
+
 public class VeiculoDTO {
-    
+    private Long id;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private Integer ano;
+    private String descricao;
+    private BigDecimal preco;
+    private Boolean disponivel;
+    private String dataCadastro;
+    private String imagem;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
 }
