f4d8a08153e7697821746deac10956d3b6399811
Milton Júnior <jrsinhorini@gmail.com>
2025-06-29 13:27:32 -0300

    Correçaõ no index para pt-br, e implementação dos componentes no App.js

--- frontcomprecar/src/App.js
+++ frontcomprecar/src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/DashBoard.jsx";
+import Veiculos from "./pages/Veiculos.jsx";
+import Funcionarios from "./pages/Funcionarios.jsx";
+import Cart from "./pages/Cart.jsx";
+import Register from "./pages/Register.jsx";
+import ListaPedidos from "./pages/ListaPedidos.jsx";
+
+import NavbarSwitcher from "./components/NavbarSwitcher.jsx";
+import PrivateRoute from "./routes/PrivateRoute.jsx";
+import PrivateRouteAdmin from "./routes/PrivateRouteAdmin.jsx";
+
+import { testConnection } from "./services/testApi";
+import "./styles/ConnectionError.css";
+import PrivateRouteAdminFuncionario from "./routes/PrivateRouteAdminFuncionario.jsx";
+import PedidosCliente from "./pages/PedidosClientes.jsx";
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [connectionError, setConnectionError] = useState("");
+
+  useEffect(() => {
+    const checkConnection = async () => {
+      try {
+        const isConnected = await testConnection();
+        setIsConnected(isConnected);
+      } catch {
+        setConnectionError(
+          "Não foi possível conectar ao servidor. Por favor, verifique se o backend está rodando."
+        );
+      }
+    };
+    checkConnection();
+  }, []);
+
+  if (!isConnected && connectionError) {
+    return (
+      <div className="connection-error">
+        <h2>{connectionError}</h2>
+        <p>Por favor, verifique se:</p>
+        <ul>
+          <li>O backend está rodando na porta 8080</li>
+          <li>O servidor não está retornando erros</li>
+        </ul>
+      </div>
+    );
+  }
+
+  const getUser = () => {
+    try {
+      return JSON.parse(localStorage.getItem("user"));
+    } catch {
+      return null;
+    }
+  };
+
+  const user = getUser();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavbarSwitcher />
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="/painel" /> : <Navigate to="/login" />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Register />} />
+
+        <Route
+          path="/painel"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/veiculos"
+          element={
+            <PrivateRouteAdminFuncionario>
+              <Veiculos />
+            </PrivateRouteAdminFuncionario>
+          }
+        />
+        <Route
+          path="/funcionarios"
+          element={
+            <PrivateRouteAdmin>
+              <Funcionarios />
+            </PrivateRouteAdmin>
+          }
+        />
+        <Route
+          path="/carrinho"
+          element={
+            <PrivateRoute>
+              <Cart />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/pedidos"
+          element={
+            <PrivateRouteAdminFuncionario>
+              <ListaPedidos />
+            </PrivateRouteAdminFuncionario>
+          }
+        />
+        <Route
+          path="/meus-pedidos"
+          element={
+            <PrivateRoute>
+              <PedidosCliente />
+            </PrivateRoute>
+          }
+        />
+
+      </Routes>
+    </>
   );
 }
 
