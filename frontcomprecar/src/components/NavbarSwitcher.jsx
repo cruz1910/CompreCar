90c4a74831a9dca57811ccb88c64db40d9b778b8
Milton Júnior <jrsinhorini@gmail.com>
2025-06-29 13:14:46 -0300

    Criação de pastas routes, navbar e api.js

---
+++ frontcomprecar/src/components/NavbarSwitcher.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import './NavbarSwitcher.css';
+
+const NavbarSwitcher = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    navigate('/');
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/painel" className="navbar-title">CompreCar</Link>
+      </div>
+      <div className="navbar-right">
+        {user && (
+          <>
+            <Link className="navbar-link" to="/painel">Início</Link>
+
+            {['FUNCIONARIO', 'ADMIN'].includes(user.tipo) && (
+              <>
+                <Link className="navbar-link" to="/veiculos">Veículos</Link>
+                <Link className="navbar-link" to="/pedidos">Pedidos</Link>
+              </>
+            )}
+
+            {user.tipo === 'ADMIN' && (
+              <Link className="navbar-link" to="/funcionarios">Funcionários</Link>
+            )}
+
+            {user.tipo === 'CLIENTE' && (
+              <>
+                <Link className="navbar-link" to="/meus-pedidos">Meus Pedidos</Link>
+                <Link to="/carrinho" className="cart-icon">
+                </Link>
+              </>
+            )}
+
+            <span className="welcome-message">
+              Bem-vindo, {user.nome || user.email}
+            </span>
+
+            <button onClick={logout} className="logout-btn">Sair</button>
+
+            {user.tipo === 'CLIENTE' && (
+              <>
+
+                <Link to="/carrinho" className="cart-icon">
+                  <FaShoppingCart size={18} />
+                </Link>
+              </>
+            )}
+
+
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarSwitcher;
